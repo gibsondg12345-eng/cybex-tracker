88f441fd64d72d0ea6a5cac603bdb650a2d1afb2
gibsondg12345-eng <gibsondg12345@gmail.com>
2026-04-25 17:35:30 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'cybex-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = 'cybex-v2';
+const ASSETS = ['./index.html', './manifest.json', './sw.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
